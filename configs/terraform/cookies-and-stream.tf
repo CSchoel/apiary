497e6170480bdee8326b2a14a4573473ddb52938
Christopher Schölzel <christopher.schoelzel@mailbox.org>
2026-01-30 10:26:17 +0100

    switches to directly downloading cloud image

--- configs/terraform/cookies-and-stream.tf
+++ configs/terraform/cookies-and-stream.tf
@@ -1,14 +1,21 @@
+# references:
+# - https://registry.terraform.io/providers/bpg/proxmox/latest/docs/resources/virtual_environment_vm
+# - https://www.youtube.com/watch?v=8D2lN7MEavM
+# - https://www.youtube.com/watch?v=sJlnXwZDdso
+# - https://registry.terraform.io/providers/bpg/proxmox/latest/docs/guides/cloud-image
 resource "proxmox_vm_qemu" "cookies-and-stream" {
   name        = "cookies-and-stream"
   description = "Debian VM for TV connection"
   node_name   = "hive"
   tags        = ["terraform", "debian"]
-  clone {
-    vm_id = 900
-    full  = true
-  }
+  # clone existing VM
+  # clone {
+  #   vm_id = 900
+  #   full  = true
+  # }
   agent {
-    enabled = true
+    # TODO: Set to true once we have qemu-guest-agent installed in cloudinit-template
+    enabled = false
   }
   cpu {
     cores = 2
@@ -23,14 +30,23 @@ resource "proxmox_vm_qemu" "cookies-and-stream" {
   # vga {
   #   type = "std"
   # }
+  # create a new scsi disk
+  # disk {
+  #   interface    = "scsi0"
+  #   size         = "100G"
+  #   file_format  = "raw"
+  #   datastore_id = "local-lvm"
+  # }
   disk {
-    interface    = "scsi0"
-    size         = "100G"
-    file_format  = "raw"
     datastore_id = "local-lvm"
+    import_from  = proxmox_virtual_environment_download_file.debian_cloud_image.id
+    interface    = "scsi0"
+    # iothread     = true
+    discard = "on"
+    size    = 100
   }
 
-  operation_system {
+  operating_system {
     type = "l26"
   }
 
@@ -46,6 +62,13 @@ resource "proxmox_vm_qemu" "cookies-and-stream" {
       password = var.vm_password
     }
   }
+}
+
+resource "proxmox_virtual_environment_download_file" "debian_cloud_image" {
+  content_type = "import"
+  datastore_id = "local"
+  node_name    = "hive"
+  url          = "https://cloud.debian.org/images/cloud/trixie/20260112-2355/debian-13-generic-amd64-20260112-2355.qcow2"
 }
 
 output "vm_ip_address" {
