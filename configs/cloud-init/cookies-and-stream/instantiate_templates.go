8eda07d2afd101ce5be11d6521d1e28d2444ce03
Christopher Schölzel <christopher.schoelzel@mailbox.org>
2026-02-03 11:59:43 +0100

    reads ssh key from disk

--- configs/cloud-init/cookies-and-stream/instantiate_templates.go
+++ configs/cloud-init/cookies-and-stream/instantiate_templates.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"os"
+	"path/filepath"
 	"text/template"
 )
 
@@ -12,7 +13,18 @@ type User struct {
 }
 
 func main() {
-	user := User{Passwd: "test", SSH_authorized_key: "test"}
+	home_dir, homedir_err := os.UserHomeDir()
+	if homedir_err != nil {
+		log.Fatalf("Could not determine home directory! %v", homedir_err)
+		return
+	}
+	ssh_pubkey := filepath.Join(home_dir, ".ssh/id_rsa.pub")
+	ssh_key, ssh_key_err := os.ReadFile(ssh_pubkey)
+	if ssh_key_err != nil {
+		log.Fatalf("Could not read the SSH public key! %v", ssh_key_err)
+		return
+	}
+	user := User{Passwd: "test", SSH_authorized_key: string(ssh_key)}
 	t1 := template.New("t1")
 	raw, err := os.ReadFile(os.Args[1])
 	if err != nil {
