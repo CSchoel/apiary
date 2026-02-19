4f5a56ef6189b4c0bcc663eef88db0d4861147c5
Christopher Schölzel <christopher.schoelzel@mailbox.org>
2026-02-19 14:40:07 +0100

    sets shell of ansible user and allows running the playbook as user

--- configs/ansible/scripts/ansuser.sh
+++ configs/ansible/scripts/ansuser.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-# Run ansible playbook with root privileges
+# Run ansible playbook with user privileges
 
 # Source - https://stackoverflow.com/a/246128
 # Posted by dogbane, modified by community. See post 'Timeline' for change history
 # Retrieved 2026-02-03, License - CC BY-SA 4.0
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-ansible-playbook -i  "$SCRIPT_DIR/../inventory/inventory_root.yml" --vault-id "main@${SCRIPT_DIR}/../scripts/gnome-keyring-client.sh" $@
+ansible-playbook -i  "$SCRIPT_DIR/../inventory/inventory.yml" --vault-id "main@${SCRIPT_DIR}/../scripts/gnome-keyring-client.sh" $@
